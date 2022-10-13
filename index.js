c289262e4e10ed1dd755e46753105dd734325a67
Jelena Djurdjevic <+>
2022-10-13 11:31:55 -0600

    commit tree 952b83e638413730383b5ccacecd3f8bcb729c98

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function createEmployeeRecord(arrayOfEmployeeInformation){
+    
+    return {
+        firstName:  arrayOfEmployeeInformation[0],
+        familyName: arrayOfEmployeeInformation[1],
+        title: arrayOfEmployeeInformation[2],
+        payPerHour: arrayOfEmployeeInformation[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    
+}
+function createEmployeeRecords(arrayOfArrays){
+   return  arrayOfArrays.map(arrayOfEmployeeInformation => createEmployeeRecord(arrayOfEmployeeInformation))
+}
+
+function createTimeInEvent(dateStamp){
+    
+}
+
+
+
+    
+
+
+
+
+
+
+
+
+// Employees always check in and check out.
+// Employees always check in and out on the hour.
+// The time is represented on a 24-hour clock (1300 is 1:00 pm); this keeps the math easier and is the standard in most of the world.
+// When timestamps are needed, they will be provided as Strings in the form: "YYYY-MM-DD 800" or "YYYY-MM-DD 1800" e.g. "2018-01-01 2300".
+// Employees will never work across days, e.g., in at 2200 and out at 0400 the next day.
